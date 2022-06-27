ceec7ba560ff8e61db3500de18bcda11c282b5f5
Erick Barauna <98670029+erickbarauna@users.noreply.github.com>
2022-06-27 19:07:46 -0300

    update 4

--- scripts.js
+++ scripts.js
@@ -77,3 +77,19 @@ document.querySelector(".close-modal").addEventListener("click", () => {
   modal.querySelector("iframe").src = ``;
   document.querySelector("body").style.overflow = "initial";
 });
+
+
+
+
+let show = true;
+
+const menuSection = document.querySelector(".menu-section")
+const menuToggle = document.querySelector(".menu-toggle")
+
+menuToggle.addEventListener("click", () => {
+
+  document.body.style.overflow = show ? "hidden" : "initial"
+
+  menuSection.classList.toggle("on", show)
+  show = !show;
+})
